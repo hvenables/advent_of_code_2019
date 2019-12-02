95843929fa4c3b4bd1d1997b612c0fc3f07402f2
Harry Venables <harry.venables@immersivelabs.co.uk>
2019-12-02 23:20:09 +0000

    fix day_2_part_1 after git weirdness

--- day_2/day_2_part_1.rb
+++ day_2/day_2_part_1.rb
@@ -1,70 +1,26 @@
-
-3.times do
-  exit
-fg
-vim
-:qcp da 2
-fg
-vimdayay noun = 1
-verb = 1
-result = 0
-
-while res19690720u
-
-3.times do
-  return
-  end
-end
-require 'pry'
 input = File.read('./input2.txt').split(',').map(&:to_i)
 # input = '1,9,10,3,2,3,11,0,99,30,40,50'.split(',').map(&:to_i)
 
 
-end
+input[1] = 12
+input[2] = 2
 
 def process_input(input, steps = 0)
-e 2 
-nd
+  intcode = input[(steps * 4)...((steps * 4) + 4)]
+  opcode = intcode[0]
 
+  case opcode
+  when 1
+    input[intcode[3]] = input[intcode[1]] + input[intcode[2]]
+  when 2
+    input[intcode[3]] = input[intcode[1]] * input[intcode[2]]
+  when 99
+    return input
+  end
+
+  process_input(input, steps + 1)
+end
 
 result = process_input(input)
-input, 
+
 puts result[0]
-input,
- p 1
-
-3.times do
-  break
-rescue
-  0
-end
-end
-def attempt_to_solve(noun = 1, verb = 1)
-  puts "attempt: noun ##{noun} verb ##{verb}"
-
-  old_noun = input[1]
-  old_verb = input[2]
-
-
-  input[1] = noun
-  input[2] = verb
-
-  result = process_input(input)
-
-
-  input[1] = old_noun
-  input[2] = old_verb
-  if result == 19690720input, 
-
-
-end
-
-  case noun
-  when 16..20
-    if verb > 15
-      puts 'no solution'
-      return  
-    end
-    attempt_to_solve(1, verb + 1)
-  when 1..15
-    attempt_to_solve(input)
