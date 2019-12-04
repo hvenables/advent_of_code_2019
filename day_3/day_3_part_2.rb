67bfa49ceef9d66ec7bbc6a751f9e703e0e9cd86
Harry Venables <harry.venables@immersivelabs.co.uk>
2019-12-04 00:31:55 +0000

    day 3 solution

---
+++ day_3/day_3_part_2.rb
@@ -0,0 +1,92 @@
+@base_map = Array.new(10000) { Array.new(10000) { '.' } }
+
+wire_1, wire_2 = File.read('./input3.txt').split("\n").map { |wire| wire.split(',') }
+
+# wire_1 = "R8,U5,L5,D3".split(',')
+# wire_2 = "U7,R6,D4,L4".split(',')
+
+# wire_1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(',')
+# wire_2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(',')
+
+# wire_1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(',')
+# wire_2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(',')
+@position = [1, 1]
+@moves = 0
+@wire = 'W1'
+@lowest = 10000000
+
+def make_move(input)
+  direction = input[0]
+  spaces = input[1..-1].to_i
+
+  case direction
+  when 'R'
+    @position[1].upto(@position[1] + (spaces - 1)).each do |y|
+      @moves += 1
+      @base_map[@position[0]][y] = if @base_map[@position[0]][y] == '.'
+                                     "#{@moves}#{@wire}"
+                                   else
+                                     distance = (@moves + @base_map[@position[0]][y][0..-3].to_i) - 2
+                                     @lowest = distance if distance < @lowest && distance != 0
+                                     p distance
+                                     distance
+                                   end
+    end
+    @position = [@position[0], @position[1] + spaces]
+  when 'L'
+    @position[1].downto(@position[1] - (spaces - 1)).each do |y|
+      @moves += 1
+      @base_map[@position[0]][y] = if @base_map[@position[0]][y] == '.'
+                                     "#{@moves}#{@wire}"
+                                   else
+                                     distance = (@moves + @base_map[@position[0]][y][0..-3].to_i) - 2
+                                     @lowest = distance if distance < @lowest && distance != 0
+                                     p distance
+                                     distance
+                                   end
+    end
+    @position = [@position[0], @position[1] - spaces]
+  when 'U'
+    @position[0].upto(@position[0] + (spaces - 1)).each do |x|
+      @moves += 1
+      @base_map[x][@position[1]] = if @base_map[x][@position[1]] == '.'
+                                     "#{@moves}#{@wire}"
+                                   else
+                                     distance = (@moves + @base_map[x][@position[1]][0..-3].to_i) - 2
+                                     @lowest = distance if distance < @lowest && distance != 0
+                                     p distance
+                                     distance
+                                   end
+    end
+    @position = [@position[0] + spaces, @position[1]]
+  when 'D'
+    @position[0].downto(@position[0] - (spaces - 1)).each do |x|
+      @moves += 1
+      @base_map[x][@position[1]] = if @base_map[x][@position[1]] == '.'
+                                     "#{@moves}#{@wire}"
+                                   else
+                                     distance = (@moves + @base_map[x][@position[1]][0..-3].to_i) - 2
+                                     @lowest = distance if distance < @lowest && distance != 0
+                                     p distance
+                                     distance
+                                   end
+    end
+    @position = [@position[0] - spaces, @position[1]]
+  end
+end
+
+wire_1.each do |move|
+  make_move(move)
+end
+
+@position = [1,1]
+@wire = 'W2'
+@moves = 0
+
+wire_2.each do |move|
+  make_move(move)
+end
+
+puts "Lowest is #{@lowest}"
+
+# puts @base_map.map { |x| x.map { |a| a.center(6) }.join(' ') }
